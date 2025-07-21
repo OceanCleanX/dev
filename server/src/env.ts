22ab72f0ff38cd9aa5d2842306342eabab231b3b
BrandenXia <xxtbranden@outlook.com>
2025-07-21 22:35:33 +0800

    remove unused env

--- server/src/env.ts
+++ server/src/env.ts
@@ -2,16 +2,6 @@ const throwIfMissing = (name: string): never => {
   throw new Error(`No env variable found for ${name}`);
 };
 
-export const TCP_REMOTE_ADDR =
-  process.env.TCP_REMOTE_ADDR ?? throwIfMissing("TCP_REMOTE_ADDR");
-export const TCP_REMOTE_PORT = parseInt(
-  process.env.TCP_REMOTE_PORT ?? throwIfMissing("TCP_REMOTE_PORT"),
-);
-export const WS_REMOTE_ADDR =
-  process.env.TCP_REMOTE_ADDR ?? throwIfMissing("TCP_REMOTE_ADDR");
-export const WS_REMOTE_PORT = parseInt(
-  process.env.TCP_REMOTE_PORT ?? throwIfMissing("TCP_REMOTE_PORT"),
-);
 export const AGORA_APP_ID =
   process.env.AGORA_APP_ID ?? throwIfMissing("AGORA_APP_ID");
 export const AGORA_APP_CERT =
