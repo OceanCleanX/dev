5e33b3eadff2ea8eb56e04cbd366275dc075b9c1
BrandenXia <xxtbranden@outlook.com>
2025-07-24 15:40:40 +0800

    cors setting

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -3,14 +3,11 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { admin, username } from "better-auth/plugins";
 
 import prisma from "@/lib/db";
+import { trustedOrigins } from "@/lib/cors";
 
 export const auth: ReturnType<typeof betterAuth> = betterAuth({
   database: prismaAdapter(prisma, { provider: "sqlite" }),
   emailAndPassword: { enabled: true },
   plugins: [admin(), username({ minUsernameLength: 6 })],
-  trustedOrigins: [
-    "http://localhost:3001",
-    "https://preview.oceancleanx.org",
-    "https://oceancleanx.org",
-  ],
+  trustedOrigins: trustedOrigins,
 });
